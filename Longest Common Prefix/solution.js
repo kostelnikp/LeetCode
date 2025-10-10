b7e2faf4229953bb30477136c3b04a2ed96c5232
kostelnikp <kostelnik.peto@gmail.com>
2025-10-10 16:46:02 +0200

    Longest Common Prefix

---
+++ Longest Common Prefix/solution.js
@@ -0,0 +1,15 @@
+let longestCommonPrefix = function(strs) {
+    let sorted = strs.sort();
+    let prefix = "";
+
+    for (let i = 0; i < sorted[0].length; i++) {
+        if (sorted[0][i] === sorted[sorted.length - 1][i]) {
+            prefix += sorted[0][i];
+        } else {
+            break;
+        }
+    }
+    return prefix;
+};
+
+console.log(longestCommonPrefix(["abab","aba","abc"])); // "ab"
